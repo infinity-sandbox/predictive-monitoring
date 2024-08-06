1958bdec21220ced77e54ffb2b18a8b605e304fd
Abel Mitiku <abelmgetnet@gmail.com>
2024-08-06 19:53:20 +0300

    completed the backend

--- dashboard/src/components/statusPages/successRegistrationPage.tsx
+++ dashboard/src/components/statusPages/successRegistrationPage.tsx
@@ -11,7 +11,7 @@ const SuccessRegistrationPage: React.FC = () => {
       <Result
         icon={<CheckCircleOutlined style={{ color: '#1591ea', fontSize: '48px' }} />}
         title="You are successfully registered!"
-        subTitle="For the upcoming September class."
+        subTitle="For the Applicare AI."
         extra={
           <Button className='success-login-button' type="primary" onClick={() => navigate('/login')}>
             Go to Login
